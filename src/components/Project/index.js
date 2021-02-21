7de0c52ef4cce262aec8f213b46d4c28fec6a4ca
Pedal <jmb181@humboldt.edu>
2021-02-21 10:29:20 -0800

    Portfolio tranistion effects completed

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import github from "../../assets/images/github_black.png";
 
 const Project = ({project}) => {
     
@@ -7,10 +8,18 @@ const Project = ({project}) => {
         <figure className="projectCard">
             
             <div className="projectText">
-            <a href={project.deploy} target="_blank"><h3>{project.name}</h3></a>
-            <p>{project.desc}</p>
+            
+                <div className="projectLinks">
+                
+                <a href={project.deploy} target="_blank"><h3>{project.name}</h3></a>
+                <a href={project.repo} target="_blank"><img className="projectLogo" alt="Github Logo" src={github}></img></a>
+                
+                </div>
+                
+                <p>{project.desc}</p>
+                
             </div>
-            <img alt={project.name} src={require(`../../assets/images/${project.image}`).default} />
+            <img className="projectBackground" alt={project.name} src={require(`../../assets/images/${project.image}`).default} />
            
         </figure>
     )
